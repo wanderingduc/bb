3eac5828b8abf3ecb395070ca8130fa7cedf028b
wanderingduc <96486111+wanderingduc@users.noreply.github.com>
2025-04-11 12:23:15 +0200

    Fix gold/death column

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// NETWORK
+const addr = 'http://localhost:8000'; // CHANGE BEFORE DEPLOY
+
+// DOM
 const ulgt = document.getElementById('gold200');
 const uldt = document.getElementById('death200');
 const ulgo = document.getElementById('gold100');
@@ -11,11 +15,24 @@ let newLi;
 
 async function meta(){
     get_old_c()
-    get_c()
+    if (before_market()){
+        display_before_market();
+    }else{
+        get_c();
+    }
+}
+
+function display_before_market(){
+    uldo.innerHTML = 'N/A';
+    uldt.innerHTML = 'N/A';
+    ulgo.innerHTML = 'N/A';
+    ulgt.innerHTML = 'N/A';
+    const now = new Date();
+    updated.innerHTML = "Updated: " + now;
 }
 
 async function get_old_c(){
-    const r = await fetch('http://13.41.110.181:8080/anal/old/', {
+    const r = await fetch(addr + '/anal/old/', {
         method: 'GET',
         mode: 'cors',
         headers: {
@@ -29,6 +46,12 @@ async function get_old_c(){
         return res.json();
     })
     .then(data => {
+
+        olduldo.innerHTML = '';
+        olduldt.innerHTML = '';
+        oldulgo.innerHTML = '';
+        oldulgt.innerHTML = '';
+
         return data.data;
     })
     .then(stocks => {
@@ -36,25 +59,45 @@ async function get_old_c(){
             switch(stocks[i].type){
                 case 'death100':{
                     newLi = document.createElement("li");
-                    newLi.textContent = stocks[i].ticker;
+                    const t = document.createElement('p');
+                    t.innerHTML = stocks[i].ticker;
+                    const d = document.createElement('p');
+                    d.innerHTML = stocks[i].date.split('T')[0];
+                    newLi.appendChild(t);
+                    newLi.appendChild(d);
                     olduldo.appendChild(newLi);
                     break;
                 }
                 case 'death200':{
                     newLi = document.createElement("li");
-                    newLi.textContent = stocks[i].ticker;
+                    const t = document.createElement('p');
+                    t.innerHTML = stocks[i].ticker;
+                    const d = document.createElement('p');
+                    d.innerHTML = stocks[i].date.split('T')[0];
+                    newLi.appendChild(t);
+                    newLi.appendChild(d);
                     olduldt.appendChild(newLi);
                     break;
                 }
                 case 'gold100':{
                     newLi = document.createElement("li");
-                    newLi.textContent = stocks[i].ticker;
+                    const t = document.createElement('p');
+                    t.innerHTML = stocks[i].ticker;
+                    const d = document.createElement('p');
+                    d.innerHTML = stocks[i].date.split('T')[0];
+                    newLi.appendChild(t);
+                    newLi.appendChild(d);
                     oldulgo.appendChild(newLi);
                     break;
                 }
                 case 'gold200':{
                     newLi = document.createElement("li");
-                    newLi.textContent = stocks[i].ticker;
+                    const t = document.createElement('p');
+                    t.innerHTML = stocks[i].ticker;
+                    const d = document.createElement('p');
+                    d.innerHTML = stocks[i].date.split('T')[0];
+                    newLi.appendChild(t);
+                    newLi.appendChild(d);
                     oldulgt.appendChild(newLi);
                     break;
                 }
@@ -117,11 +160,11 @@ async function get_c(){
         }
     }
 
-    const r = await fetch('http://13.41.110.181:8080/anal/', {
+    const r = await fetch(addr + '/anal/', {
         method: 'GET',
         mode: 'cors',
         headers: {
-            'Accept': 'application/json',
+            'Accept': 'application/json'
             }
         }
     )
@@ -137,9 +180,13 @@ async function get_c(){
         uldt.innerHTML = '';
         ulgo.innerHTML = '';
         ulgt.innerHTML = '';
-        return data.data
+        return data
     })
     .then(stocks => {
+
+        console.log(stocks.meta)
+
+        stocks = stocks.data
 
         const now = new Date();
         const y = now.getFullYear();
@@ -188,6 +235,7 @@ async function get_c(){
         console.log("Done")
     })
     .catch(e => {
+        updated.innerHTML = "Error fetching stock data";
         console.error(e)
     })
 }
@@ -203,6 +251,17 @@ function isToday(date){
         return false
     }
     return true
+}
+
+function before_market(){
+    const now = new Date();
+    const h = now.getHours();
+    const m = now.getMinutes();
+
+    if (h < 7 || (h === 7 && m < 30)){
+        return true;
+    }
+    return false;
 }
 
 function timecheck(){
